bd166fee90f267f265cb249e579eebbb064ef533
MadameGeorge <magda.powalisz@gmail.com>
2020-06-10 19:29:24 +0100

    feat: Add html webpack plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,2 +1,10 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/client/index.js',
+    plugins: [
+        new HtmlWebpackPlugin()
+    ]
+}
