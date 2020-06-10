1f5c590ad6c16d58bd718e7ee2afb30dbd7cf2f4
MadameGeorge <magda.powalisz@gmail.com>
2020-06-10 23:29:45 +0100

    feat: Add env and Aylien API keys

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,6 @@
+// Express
 const express = require('express');
 const app = express();
-const port = 8080;
 
 // Middleware
 const bodyParser = require('body-parser');
@@ -12,10 +12,32 @@ app.use(cors());
 
 // Initiate server
 app.use(express.static('dist'));
+const port = 8080;
 
 const server = app.listen(port, () => {
     console.log('Success. The server is running on localhost:', port);
 });
+
+// Env
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Aylien
+const AylienTextApi = require('aylien_textapi');
+
+const textApi = new AylienTextApi({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+});
+
+
+// textapi.sentiment({
+//     'text': 'John is a very good football player!'
+//   }, function(error, response) {
+//     if (error === null) {
+//       console.log(response);
+//     }
+//   });
 
 // Get route
 app.get('/get', getFunction);
