4073f30812c6ea0a9c215287b4678d5de0b2b821
MadameGeorge <magda.powalisz@gmail.com>
2020-06-10 20:45:16 +0100

    feat: Add webpack dev server

--- webpack.dev.js
+++ webpack.dev.js
@@ -24,21 +24,15 @@ module.exports = merge(common, {
     plugins: [
         new CleanWebpackPlugin({
              // Simulate the removal of files
-            //
             dry: true,
 
             // Write Logs to Console
-            // (Always enabled when dry is true)
-            //
             verbose: true,
 
             // Automatically remove all unused webpack assets on rebuild
-            //
             cleanStaleWebpackAssets: true,
 
             // Do not allow removal of current webpack assets
-            //
-            // default: true
             protectWebpackAssets: false,
         }),
     ]
