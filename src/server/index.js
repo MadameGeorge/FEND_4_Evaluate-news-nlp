42619dea9d0d91ad99325373438eb947811e09e7
MadameGeorge <magda.powalisz@gmail.com>
2020-06-10 18:52:56 +0100

    feat: Create express server, add reset css, add html

---
+++ src/server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+// Middleware
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const cors = require('cors');
+app.use(cors());
+
+// Initiate server
+app.use(express.static('src/client'));
+
+const server = app.listen(port, () => {
+    console.log('Success. The server is running on localhost:', port);
+});
+
+app.get('/get', getFunction);
+function getFunction(request, response) {
+    console.log(request);
+    response.send(database);
+};
+
+const database = {
+    test: 'this is a test'
+};
