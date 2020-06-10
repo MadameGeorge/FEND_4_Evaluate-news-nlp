369a4a4de571be95cf912666892f95e6c12be9f1
MadameGeorge <magda.powalisz@gmail.com>
2020-06-10 19:59:45 +0100

    feat: Add clean webpack plugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,5 +2,31 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-    mode: 'development'
+    mode: 'development',
+    output: {
+        libraryTarget: 'var',
+		library: 'Client'
+	},
+    devtool: 'source-map',
+    plugins: [
+        new CleanWebpackPlugin({
+             // Simulate the removal of files
+            //
+            dry: true,
+
+            // Write Logs to Console
+            // (Always enabled when dry is true)
+            //
+            verbose: true,
+
+            // Automatically remove all unused webpack assets on rebuild
+            //
+            cleanStaleWebpackAssets: true,
+
+            // Do not allow removal of current webpack assets
+            //
+            // default: true
+            protectWebpackAssets: false,
+        }),
+    ]
 });
