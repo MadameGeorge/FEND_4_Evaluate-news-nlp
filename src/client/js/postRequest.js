95554fe486216f692194288aa4ad8fef686c66f8
MadameGeorge <magda.powalisz@gmail.com>
2020-06-23 23:59:22 +0100

    feat: Display alert when fetch fails

--- src/client/js/postRequest.js
+++ src/client/js/postRequest.js
@@ -1,21 +1,20 @@
-export function postRequest(userInput){
-    // const fetch = require("node-fetch")
-    return fetch('http://localhost:8081/sentiment', {
+export async function postRequest(url, userInput){
+    const res = await fetch(url, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(userInput)
-    })
-    .then( res => res.json() )
-    .then( (res) => {
-        document.querySelector('.results__card').classList.remove('hidden');
-        document.getElementById('results').innerHTML = 'Results:';
-        document.getElementById('emotion').innerHTML = `Emotion: ${res.polarity}`;
-        document.getElementById('emotion-confidence').innerHTML = `Emotion confidence: ${res.polarity_confidence}`;
-        document.getElementById('subjectivity').innerHTML = `Subjectivity: ${res.subjectivity}`;
-        document.getElementById('subjectivity-confidence').innerHTML = `Subjectivity confidence: ${res.subjectivity_confidence}`;
-
-        return res;
+    }).catch( (error) => {
+        console.log('fetch error 1', error);
     });
+    try {
+        const sentiment = await res.json();
+        return sentiment;
+    }
+    catch(error) {
+        console.log('fetch error 2', error);
+        document.getElementById('error__alert').classList.remove('hidden');
+        document.getElementById('error__alert').innerHTML = 'There was a problem with API call, please try again.';
+    }
 }
